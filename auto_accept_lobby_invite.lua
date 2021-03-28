3f37f3bdf0cd3c1a53348ee8b1afa8b1b355bb31
bean07 <vietkhanh2007@gmail.com>
2021-03-28 22:26:06 +0700

    Update auto_accept_lobby_invite.lua

--- auto_accept_lobby_invite.lua
+++ auto_accept_lobby_invite.lua
@@ -1,11 +1,51 @@
 -- credits: sapphyrus (https://www.unknowncheats.me/forum/2794104-post11.html)
 
 local js = panorama.open()
-local PartyBrowserAPI = js.PartyBrowserAPI 
+local PartyBrowserAPI = js.PartyBrowserAPI
 
-local xuids = {
-    "765611xxxxxxxxxxx", -- example
-}
+local xuids = database.read("xuids") or {}
+
+client.set_event_callback("console_input", function(cmd)
+	if cmd:sub(1, 8) == "add_xuid" then
+		if cmd:len() > 9 then
+			local xuid = cmd:sub(10, -1)
+			local source = "steamid64"
+
+			-- basic "possibly a friend code" check
+			if xuid:len() == 10 and xuid:sub(6, 6) == "-" then
+				xuid = FriendsListAPI.GetXuidFromFriendCode(xuid)
+				source = "friendcode"
+            elseif xuid:find("^https://steamcommunity.com/profiles/") then
+				xuid = xuid:gsub("https://steamcommunity.com/profiles/", "")
+			end
+
+			-- very basic check if its a valid steamid64
+			if type(xuid) == "string" and xuid:len() == 17 and xuid:sub(1, 3) == "765" then
+				table.insert(xuids, xuid)
+                database.write("xuids", xuids)
+			else
+				client.error_log("Please enter a valid steam64 id or friendcode")
+			end
+		end
+
+		return true
+	elseif cmd:sub(1, 9) == "list_xuid" then
+        for _,v in next, xuids do
+            print(v)
+        end
+        return true
+    elseif cmd:sub(1, 11) == "remove_xuid" then
+        if cmd:len() > 12 then
+            local xuid = cmd:sub(13, -1)
+            for _,v in next, xuids do
+                if v == xuid then
+                    table.remove(xuids, _)
+                    database.write("xuids", xuids)
+                end
+            end
+        end
+    end
+end)
 
 local function joinByLeaderXuid(xuid)
     if PartyBrowserAPI.GetInvitesCount() == 0 then return end
@@ -13,13 +53,14 @@ local function joinByLeaderXuid(xuid)
         local lobby_xuid = PartyBrowserAPI.GetInviteXuidByIndex(i)
         if tostring(PartyBrowserAPI.GetPartyMemberXuid(lobby_xuid, 0)) == xuid then
             PartyBrowserAPI.ActionJoinParty(lobby_xuid)
-            print("Joined lobby of " + lobby_xuid)
         end
     end
 end
 
-client.set_event_callback("paint_ui", function() -- 
-    for _,v in next, xuids do
-        joinByLeaderXuid(v)
+client.set_event_callback("paint_ui", function()
+    if not entity.get_local_player() then
+        for _,v in next, xuids do
+            joinByLeaderXuid(v)
+        end
     end
 end)
